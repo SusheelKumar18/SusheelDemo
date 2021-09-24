39c9a06fa70e60c2ffcdccc2581e1869865631d9
SusheelKumar18 <kumar.susheel543@gmail.com>
2021-09-24 10:25:53 -0400

    susheel commit 1

--- src/main/java/com/susheel/listeners/ListenerTest.java
+++ src/main/java/com/susheel/listeners/ListenerTest.java
@@ -6,41 +6,41 @@ import org.testng.ITestResult;
 
 public class ListenerTest implements ITestListener {
 
-	public void onFinish(ITestContext arg0) {
+	public void onFinish(ITestContext Result) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	public void onStart(ITestContext arg0) {
+	public void onStart(ITestContext Result) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onTestFailedButWithinSuccessPercentage(ITestResult Result) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onTestFailure(ITestResult Result) {
-		System.out.println("The name of the testcase failed is :"+Result.getName());		
+		System.out.println("The name of the testcase failed is :" + Result.getName());
 	}
 
 	public void onTestSkipped(ITestResult Result) {
-		
-		System.out.println("The name of the testcase Skipped is :"+Result.getName());
-	
+
+		System.out.println("The name of the testcase Skipped is :" + Result.getName());
+
 	}
 
 	public void onTestStart(ITestResult Result) {
-		
-		System.out.println(Result.getName()+" test case started");
-		
+
+		System.out.println(Result.getName() + " test case started");
+
 	}
 
 	public void onTestSuccess(ITestResult Result) {
 
-		System.out.println("The name of the testcase passed is :"+Result.getName());
-		
+		System.out.println("The name of the testcase passed is :" + Result.getName());
+
 	}
 
 }
